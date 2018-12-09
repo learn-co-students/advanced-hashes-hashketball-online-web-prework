ef92c05a38271b4e5431882b767d9b876cb3be1f
Robert McCary <robert.mccary@yahoo.com>
2018-12-09 20:58:39 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,44 +1,90 @@
 def game_hash
-  game = {
+  {
   home: {
     team_name: "Brooklyn Nets",
-    colors: ["Black, White"],
+    colors: ["Black", "White"],
       players: {
-        name: "Alan Anderson", number: 0, shoe: 16, points: 22,rebounds: 12,
-        assists: 12, steals: 3, blocks: 1, slam_dunks: 1,
+        "Alan Anderson" => { number: 0, shoe: 16, points: 22,rebounds: 12,
+        assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
 
-        name: "Reggie Evans", number: 30, shoe: 14, points: 12, rebounds: 12,
-          assists: 12, steals: 12, blocks: 12, slam_dunks: 7,
+        "Reggie Evans" => { number: 30, shoe: 14, points: 12, rebounds: 12,
+          assists: 12, steals: 12, blocks: 12, slam_dunks: 7},
 
-        name: "Brook Lopez", number: 11, shoe: 17, points: 17, rebounds: 19,
-          assists: 10, steals: 3, blocks: 1, slam_dunks: 15,
+        "Brook Lopez" => {number: 11, shoe: 17, points: 17, rebounds: 19,
+          assists: 10, steals: 3, blocks: 1, slam_dunks: 15},
 
-        name: "Mason Plumlee", number: 1, shoe: 19, points: 26, rebounds: 12 ,
-          assists: 6, steals: 3, blocks: 8, slam_dunks: 5,
+        "Mason Plumlee" => { number: 1, shoe: 19, points: 26, rebounds: 12 ,
+          assists: 6, steals: 3, blocks: 8, slam_dunks: 5},
 
-        name: "Jason Terry", number: 31, shoe: 15, points: 19, rebounds: 2 ,
-            assists: 1, steals: 4, blocks: 11, slam_dunks: 1}},
+        "Jason Terry" => {number: 31, shoe: 15, points: 19, rebounds: 2 ,
+            assists: 2, steals: 4, blocks: 11, slam_dunks: 1}
+          }
+        },
 away: {
   team_name: "Charlotte Hornets",
-    colors: ["Turquoise, Purple"],
+    colors: ["Turquoise", "Purple"],
       players: {
-        name:"Jeff Adrien", number: 4, shoe: 18, points: 10, rebounds: 1,
-          assists: 1, steals: 2, blocks: 7, slam_dunks: 2,
+        "Jeff Adrien" => {number: 4, shoe: 18, points: 10, rebounds: 1,
+          assists: 1, steals: 2, blocks: 7, slam_dunks: 2},
 
-        name: "Bismak Biyombo", number: 0, shoe: 16, points: 12, rebounds: 4,
-          assists: 7, steals: 7, blocks: 15, slam_dunks:10,
+        "Bismak Biyombo" => {number: 0, shoe: 16, points: 12, rebounds: 4,
+          assists: 7, steals: 7, blocks: 15, slam_dunks:10},
 
-        name: "DeSagna Diop", number: 2, shoe: 14, points: 24, rebounds: 12,
-          assists: 12, steals: 4, blocks: 5, slam_dunks:5,
+        "DeSagna Diop" => {number: 2, shoe: 14, points: 24, rebounds: 12,
+          assists: 12, steals: 4, blocks: 5, slam_dunks:5},
 
-        name: "Ben Gordon", number: 8, shoe: 15, points: 33, rebounds: 3,
-          assists: 2, steals: 1, blocks: 1, slam_dunks: 0,
+        "Ben Gordon" => {number: 8, shoe: 15, points: 33, rebounds: 3,
+          assists: 2, steals: 1, blocks: 1, slam_dunks: 0},
 
-        name: "Brendan Haywood", number: 33, shoe: 15, points: 6, rebounds: 12,
-            assists: 12, steals: 22, blocks: 5, slam_dunks: 12}}}
+        "Brendan Haywood" => {number: 33, shoe: 15, points: 6, rebounds: 12,
+            assists: 12, steals: 22, blocks: 5, slam_dunks: 12}}}}
 end
 
-# def num_points_scored(players_name)
-#   game.collect do |k, v|
-#     if k == name:
+def players
+  game_hash[:home][:players].merge(game_hash[:away][:players])
+end
+
+def find_player(name)
+  players.fetch(name)
+end
+
+def num_points_scored(name)
+  find_player(name).fetch(:points)
+end
+
+def shoe_size(name)
+  find_player(name).fetch(:shoe)
+end
+
+def team
+  game_hash.values
+end
+
+def find_team(team_name)
+  team.find {|x| x.fetch(:team_name) == team_name}
+end
+
+def team_colors(team_name)
+  team = find_team(team_name)
+  team.fetch(:colors)
+end
+
+def team_names
+  team.map {|x| x.fetch(:team_name)}
+end
+
+def player_numbers(team_name)
+  find_team(team_name)[:players].map {|player, stats| stats[:number] }
+end
+
+def player_stats(name)
+  find_player(name)
+end
+
+def big_shoe
+  players.max_by{|player, stats| stats.fetch(:shoe)}[1]
+end
+
+def big_shoe_rebounds
+  big_shoe.fetch(:rebounds)
 end
