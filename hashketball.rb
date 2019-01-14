fe3158a600f70cfcaf1b7f15b637c68ffe43779d
A Glazer <avigayilglazer@gmail.com>
2019-01-14 09:57:11 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -2,10 +2,10 @@ require 'pry'
 
 def game_hash
 game = {
-  :home => {
-    :team_name => "Brooklyn Nets",
-    :colors => ["black", "white"],
-    :players => {
+  home: {
+    team_name: "Brooklyn Nets",
+    colors: ["Black", "White"],
+    players: {
       "Alan Anderson" => {
         number: "0",
         shoe: "16",
@@ -57,8 +57,63 @@ game = {
         slam_dunks: 1
       }
     }
+  },
+  away:{
+    team_name: "Charlotte Hornets",
+    colors: ["Turquoise", "Purple"],
+    players: {
+      "Jeff Adrien" => {
+        number: "4",
+        shoe: "18",
+        points: 10,
+        rebounds: 1,
+        assists: 1,
+        steals: 2,
+        blocks: 7,
+        slam_dunks: 2
+      },
+      "Bismark Biyombo" => {
+        number: "0",
+        shoe: "16",
+        points: 12,
+        rebounds: 4,
+        assists: 7,
+        steals: 7,
+        blocks: 15,
+        slam_dunks: 10
+      },
+      "DeSagna Diop" => {
+        number: "2",
+        shoe: "14",
+        points: 24,
+        rebounds: 12,
+        assists: 12,
+        steals: 4,
+        blocks: 5,
+        slam_dunks: 5
+      },
+      "Ben Gordon" => {
+        number: "8",
+        shoe: "15",
+        points: 33,
+        rebounds: 3,
+        assists: 2,
+        steals: 1,
+        blocks: 1,
+        slam_dunks: 0
+      },
+      "Brendan Haywood" => {
+        number: "33",
+        shoe: "15",
+        points: 6,
+        rebounds: 12,
+        assists: 12,
+        steals: 22,
+        blocks: 5,
+        slam_dunks: 12
+      }
+    }
   }
-
 
 }
 
