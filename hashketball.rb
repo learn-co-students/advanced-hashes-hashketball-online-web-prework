8cc88d55a4f31ed0af3c45a61a78c783824b074f
Vinh Do <vinhdo07@gmail.com>
2019-01-20 20:51:43 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,22 @@
-# Write your code here!
+require 'pry'
 
 
+def game_hash
+{
+    home:
+      {:team_name => [],
+      :colors => [],
+      :players => {}
+      },
 
+    away:
+      {:team_name => [],
+      :colors => [],
+      :players => {}
+      }
+  }
+end
 
-
-
-
-
-
-
+def num_points_scored(player)
+  binding.pry
+end
