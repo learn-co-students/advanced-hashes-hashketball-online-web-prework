399c27c772a45bdf0a36a3cc282a0d328c04082d
Christoph Gruener <christophgruener@me.com>
2019-02-05 19:08:00 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,20 @@
 # Write your code here!
 
-
+def game_hash
+  
+  game_hash = {  :home => {
+              :team_name => "", 
+              :colors => [], 
+              :players => {}
+  }, 
+            :away => {
+              :team_name => {},
+              :colors => {},
+              :players => {}
+            }   
+    
+  }
+            
 
 
 
