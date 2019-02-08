252c29012d831ee8ea20696d5d23cfeafb7ccf97
Eyal Toledano <eyaltoledano@me.com>
2019-02-08 17:13:52 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -7,54 +7,54 @@ def game_hash
     colors: ["Black", "White"],
     players: {
       "Alan Anderson" => {
-        number: "0",
-        shoe: "16",
-        points: "22",
-        rebounds: "12",
-        assists: "12",
-        steals: "3",
-        blocks: "1",
-        slam_dunks: "1"
+        number: 0,
+        shoe: 16,
+        points: 22,
+        rebounds: 12,
+        assists: 12,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 1
       },
       "Reggie Evans" => {
-        number: "30",
-        shoe: "14",
-        points: "12",
-        rebounds: "12",
-        assists: "12",
-        steals: "12",
-        blocks: "12",
-        slam_dunks: "7"
+        number: 30,
+        shoe: 14,
+        points: 12,
+        rebounds: 12,
+        assists: 12,
+        steals: 12,
+        blocks: 12,
+        slam_dunks: 7
       },
       "Brook Lopez" => {
-        number: "11",
-        shoe: "17",
-        points: "17",
-        rebounds: "19",
-        assists: "10",
-        steals: "3",
-        blocks: "1",
-        slam_dunks: "15"
+        number: 11,
+        shoe: 17,
+        points: 17,
+        rebounds: 19,
+        assists: 10,
+        steals: 3,
+        blocks: 1,
+        slam_dunks: 15
       },
       "Mason Plumlee" => {
-        number: "1",
-        shoe: "19",
-        points: "26",
-        rebounds: "12",
-        assists: "6",
-        steals: "3",
-        blocks: "8",
-        slam_dunks: "5"
+        number: 1,
+        shoe: 19,
+        points: 26,
+        rebounds: 12,
+        assists: 6,
+        steals: 3,
+        blocks: 8,
+        slam_dunks: 5
       },
       "Jason Terry" => {
-        number: "31",
-        shoe: "15",
-        points: "19",
-        rebounds: "2",
-        assists: "2",
-        steals: "4",
-        blocks: "11",
-        slam_dunks: "1"
+        number: 31,
+        shoe: 15,
+        points: 19,
+        rebounds: 2,
+        assists: 2,
+        steals: 4,
+        blocks: 11,
+        slam_dunks: 1
       }
       }
     },
@@ -63,54 +63,54 @@ def game_hash
     colors: ["Turquoise", "Purple"],
     players: {
       "Jeff Adrien" => {
-        number: "4",
-        shoe: "18",
-        points: "10",
-        rebounds: "1",
-        assists: "1",
-        steals: "2",
-        blocks: "7",
-        slam_dunks: "2"
+        number: 4,
+        shoe: 18,
+        points: 10,
+        rebounds: 1,
+        assists: 1,
+        steals: 2,
+        blocks: 7,
+        slam_dunks: 2
       },
       "Bismak Biyombo" => {
-        number: "0",
-        shoe: "16",
-        points: "12",
-        rebounds: "4",
-        assists: "7",
-        steals: "7",
-        blocks: "15",
-        slam_dunks: "10"
+        number: 0,
+        shoe: 16,
+        points: 12,
+        rebounds: 4,
+        assists: 7,
+        steals: 7,
+        blocks: 15,
+        slam_dunks: 10
       },
       "DeSagna Diop" => {
-        number: "2",
-        shoe: "14",
-        points: "24",
-        rebounds: "12",
-        assists: "12",
-        steals: "4",
-        blocks: "5",
-        slam_dunks: "5"
+        number: 2,
+        shoe: 14,
+        points: 24,
+        rebounds: 12,
+        assists: 12,
+        steals: 4,
+        blocks: 5,
+        slam_dunks: 5
       },
       "Ben Gordon" => {
-        number: "8",
-        shoe: "15",
-        points: "33",
-        rebounds: "3",
-        assists: "2",
-        steals: "1",
-        blocks: "1",
-        slam_dunks: "0"
+        number: 8,
+        shoe: 15,
+        points: 33,
+        rebounds: 3,
+        assists: 2,
+        steals: 1,
+        blocks: 1,
+        slam_dunks: 0
       },
       "Brendan Haywood" => {
-        number: "33",
-        shoe: "15",
-        points: "6",
-        rebounds: "12",
-        assists: "12",
-        steals: "22",
-        blocks: "5",
-        slam_dunks: "12"
+        number: 33,
+        shoe: 15,
+        points: 6,
+        rebounds: 12,
+        assists: 12,
+        steals: 22,
+        blocks: 5,
+        slam_dunks: 12
       }
       }
     }
@@ -124,6 +124,7 @@ def num_points_scored(player_name)
         data.each do |player, stats|
           if player == player_name
             points = stats[:points]
+            return points
           end
         end
       end
@@ -131,5 +132,86 @@ def num_points_scored(player_name)
   end
 end
 
-# points = data[player_name][:points]
-# for some reason this doesn't iterate through the second location (away)
+def shoe_size(player_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |player, stats|
+          if player == player_name
+            shoe_size = stats[:shoe]
+            return shoe_size
+          end
+        end
+      end
+    end
+  end
+end
+
+def team_colors(team_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if data == team_name
+        return team_data[:colors]
+      end
+    end
+  end
+end
+
+def team_names
+  team_names = []
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :team_name
+        team_names << data
+      end
+    end
+  end
+  team_names
+end
+
+def player_numbers(team_name)
+  player_numbers = []
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if data == team_name
+        team_data[:players].each do |player,stats|
+          player_numbers << stats[:number]
+        end
+      end
+    end
+  end
+  player_numbers
+end
+
+def player_stats(player_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |player, stats|
+          if player == player_name
+            return stats
+          end
+        end
+      end
+    end
+  end
+end
+
+# First, find the player with the largest shoe size
+# Then, return that player's number of rebounds
+
+def big_shoe_rebounds
+  bigfoot = []
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players
+        data.each do |player, stats|
+          # iterate through each pair and find the biggest stats[:shoe]
+          # maybe sort the hash by the biggest stats[:shoe] then pick first?
+          binding.pry
+          stats[:shoe]
+        end
+      end
+    end
+  end
+end
