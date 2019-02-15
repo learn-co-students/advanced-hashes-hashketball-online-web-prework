1b645cd4a6082db71485a59221f4d9d4ea7465b6
Blaine Lawson <blaine.lawson@gmail.com>
2019-02-15 22:18:03 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,4 @@
-# Write your code here!
+require "pry"
 
 def game_hash
   {
@@ -15,8 +15,8 @@ def game_hash
           :steals => 3,
           :blocks => 1,
           :slam_dunks => 1
-        }
-        "Reggie Davis" => {
+        },
+        "Reggie Evans" => {
           :number => 30,
           :shoe => 14,
           :points => 12,
@@ -25,7 +25,7 @@ def game_hash
           :steals => 12,
           :blocks => 12,
           :slam_dunks => 7
-        }
+        },
         "Brook Lopez" => {
           :number => 11,
           :shoe => 17,
@@ -35,7 +35,7 @@ def game_hash
           :steals => 3,
           :blocks => 1,
           :slam_dunks => 15 
-        }
+        },
         "Mason Plumlee" => {
           :number => 1,
           :shoe => 19,
@@ -45,7 +45,7 @@ def game_hash
           :steals => 3,
           :blocks => 8,
           :slam_dunks => 5 
-        }
+        },
         "Jason Terry" => {
           :number => 31,
           :shoe => 15,
@@ -71,7 +71,7 @@ def game_hash
           :steals => 2,
           :blocks => 7,
           :slam_dunks => 2
-        }
+        },
         "Bismak Biyombo" => {
           :number => 0,
           :shoe => 16,
@@ -81,7 +81,7 @@ def game_hash
           :steals => 7,
           :blocks => 15,
           :slam_dunks => 10
-        }
+        },
         "DeSagna Diop" => {
           :number => 2,
           :shoe => 14,
@@ -91,7 +91,7 @@ def game_hash
           :steals => 4,
           :blocks => 5,
           :slam_dunks => 5 
-        }
+        },
         "Ben Gordon" => {
           :number => 8,
           :shoe => 15,
@@ -101,7 +101,7 @@ def game_hash
           :steals => 1,
           :blocks => 1,
           :slam_dunks => 0 
-        }
+        },
         "Brendan Haywood" => {
           :number => 33,
           :shoe => 15,
@@ -111,16 +111,95 @@ def game_hash
           :steals => 22,
           :blocks => 5,
           :slam_dunks => 12 
-        } 
         }
       }
     }
   }
 end
 
+# def good_practices
+#   game_hash.each do |location, team_data|
+#     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+#     binding.pry
+#       team_data.each do |attribute, data|
+#         #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+#         binding.pry
+ 
+#         #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
+#         data.each do |data_item|
+#             binding.pry
+#       end
+#     end
+#   end
+# end
 
+# good_practices
 
+def num_points_scored(player_name)
+  player_points = nil
+  
+  game_hash.each do |location, team_data|
+    team_data.each do |ind_data, attributes|
+      if attributes.class == Hash 
+        if attributes.keys.include?(player_name)
+          player_points = attributes[player_name][:points]
+        end
+      end
+    end
+  end
+  player_points
+end
 
+def shoe_size(player_name)
+  player_shoe = nil
+  
+  game_hash.each do |location, team_data|
+    team_data.each do |ind_data, attributes|
+      if attributes.class == Hash 
+        if attributes.keys.include?(player_name)
+          player_shoe = attributes[player_name][:shoe]
+        end
+      end
+    end
+  end
+  player_shoe
+end
 
+def team_colors(name_of_team)
+  colors = nil
+  
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == name_of_team
+      colors = team_data[:colors]
+    end
+  end
+  colors
+end
 
+def team_names
 
+  game_hash.collect do |location, team_data|
+      team_data[:team_name]
+  end
+end
+
+def player_numbers(team_name)
+  game_hash.collect do |location, team_data|
+
+    if team_data[:team_name].include?(team_name)
+      team_data[:players].collect do |player, stats|
+        stats[:number]
+      end
+    end
+  end.flatten.compact.sort!
+end
+
+def player_stats(name)
+  game_hash.collect do |location, team_data|
+    if team_data[:players].keys.include?(name)
+      team_data[:players][name]
+    end
+  end.flatten.compact
+end
+
+      
