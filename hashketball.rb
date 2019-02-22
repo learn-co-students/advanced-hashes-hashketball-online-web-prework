4bd20f4fe772570cf24a83ca4eb36e61430a9859
Kat B <katreesebarnes@gmail.com>
2019-02-22 19:49:09 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,7 @@
 def game_hash
   game_hash = 
   {:home =>
-  {  :team_name => "Brooklyn Nets",
+   { :team_name => "Brooklyn Nets",
      :colors => ["Black","White"], 
      :players => [{
        :player_name => "Alan Anderson" , 
@@ -64,44 +64,47 @@ def game_hash
        :steals => 2, 
        :blocks => 7, 
        :slam_dunks => 2},{
-       :player_name => , 
-        :number => , 
-        :shoe => , 
-        :points => , 
-        :rebounds => , 
-        :assists => , 
-        :steals => , 
-        :blocks => , 
-        :slam_dunks => },  {
-        :player_name => , 
-        :number => , 
-        :shoe => , 
-        :points => , 
-        :rebounds => , 
-        :assists => , 
-        :steals => , 
-        :blocks => , 
-        :slam_dunks => }, {
-        :player_name => , 
-        :number => , 
-        :shoe => , 
-        :points => , 
-        :rebounds => , 
-        :assists => , 
-        :steals => , 
-        :blocks => , 
-        :slam_dunks => }, {
-        :player_name => , 
-        :number => , 
-        :shoe => , 
-        :points => , 
-        :rebounds => , 
-        :assists => , 
-        :steals => , 
-        :blocks => , 
-        :slam_dunks => }]
-    
+       :player_name => "Bismak Biyombo", 
+        :number => 0, 
+        :shoe => 16, 
+        :points => 12, 
+        :rebounds => 4, 
+        :assists => 7, 
+        :steals => 7, 
+        :blocks => 15, 
+        :slam_dunks => 10},  {
+        :player_name => "DeSagna Diop", 
+        :number => 2, 
+        :shoe => 14, 
+        :points => 24, 
+        :rebounds => 12, 
+        :assists => 12, 
+        :steals => 4, 
+        :blocks => 5, 
+        :slam_dunks => 5}, {
+        :player_name => "Ben Gordon", 
+        :number => 8, 
+        :shoe => 15, 
+        :points => 33, 
+        :rebounds => 3, 
+        :assists => 2, 
+        :steals => 1, 
+        :blocks => 1, 
+        :slam_dunks => 0}, {
+        :player_name => "Brendan Haywood", 
+        :number => 33, 
+        :shoe => 15, 
+        :points => 6, 
+        :rebounds => 12, 
+        :assists => 12, 
+        :steals => 22, 
+        :blocks => 5, 
+        :slam_dunks => 12
+          
       }
+    ]
+   }
+  }    
   
 end
 
