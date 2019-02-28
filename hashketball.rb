0f26d24998db9f6ad170a874865e8d6339465aee
Charlotte Neff <charlotte.neff@gmail.com>
2019-02-28 21:22:57 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,17 @@
-# Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => "", 
+      :colors => [],
+      :players => {}
+    },
+    :away => {
+      :team_name => "",
+      :colors => {},
+      :players => {}
+    }
+    }
+    # Write your code here!
 
 
 
