2d93a8edb1468e32520cc06d3a36913583d14dbf
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-13 17:20:48 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -119,6 +119,22 @@ def game_hash
     }
   end
   
+def teams
+  game_hash.values
+end
+
+def players
+  game_hash[:home][:players].merge(game_hash[:away][:players])
+end
+
+def find_the_player(name)
+  players.fetch(name)
+end
+
+def player_stats(player_name)
+  find_the_player(player_name)
+end
+
 def num_points_scored(name)
   player = find_the_player(name)
   player.fetch(:points)
@@ -129,50 +145,30 @@ def shoe_size(name)
   player.fetch(:shoe)
 end
 
-def team_colors(team_name)
-  team = find_the_team(team_name)
-  team.fetch(:colors)
-end
-
 def team_names
   teams.map{|t| t.fetch(:team_name)}
-end
-
-def player_numbers(team_name)
-  find_the_team(team_name)[:players].map{ |player_name, stats| stats[:number] }
-end
-
-def player_stats(player_name)
-  find_the_player(player_name)
-end
-
-def big_shoe_rebounds
-  player_biggest_shoe_size.fetch(:rebounds)
-end
-
-def teams
-  game_hash.values
-end
-
-def players
-  game_hash[:home][:players].merge(game_hash[:away][:players])
 end
 
 def find_the_team(team_name)
   teams.find {|team| team.fetch(:team_name) == team_name}
 end
 
-def find_the_player(name)
-  players.fetch(name)
+def team_colors(team_name)
+  team = find_the_team(team_name)
+  team.fetch(:colors)
+end
+
+def player_numbers(team_name)
+  find_the_team(team_name)[:players].map{ |player_name, stats| stats[:number] }
 end
 
 def player_biggest_shoe_size
   players.max_by{|player, stats| stats.fetch(:shoe)}[1]
 end
-    # Write your code here!
 
-
-
+def big_shoe_rebounds
+  player_biggest_shoe_size.fetch(:rebounds)
+end
 
 
 
