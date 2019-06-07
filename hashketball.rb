15ce7b4e6fbd0447ad7e4598e2170e1743fb3145
Khristian Punzalan <khrispunzalan@gmail.com>
2019-06-07 13:36:16 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -174,7 +174,7 @@ def player_numbers(team_name)
   nums
 end
 
-def player_stats(player_name)
+def player_stats(players_name)
   new_hash = {}
   game_hash.each do |place, team|
     team.each do |attributes, data|
