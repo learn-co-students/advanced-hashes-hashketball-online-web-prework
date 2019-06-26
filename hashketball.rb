a9c2f70b00eb0c765c53d1f54a4edd03a6f0d0d9
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-26 01:29:08 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def game_hash
   {    home: { team_name: 'Brooklyn Nets',
               colors: %w[Black White], #%w(foo bar) is a shortcut for ["foo", "bar"]
@@ -99,3 +101,113 @@ def game_hash
             ] }
 }
 end
+
+def num_points_scored(specific_player) #Players are inputed into this parameter to find the points they scored
+  # binding.pry
+    #Going down the rabbit whole of the game_hash to access the players points
+  game_hash.each do |location, team_data| #location accesses :home and :away  /// team_data is the next hash level, the entire data for each team
+    # binding.pry
+    team_data[:players].each do |player| #this accesses the players array of hashes, player is each individual player hash
+      # binding.pry
+      if player[:player_name] == specific_player #if player_name is equal to the specific_player entered in this method, return that players points
+        return player[:points]
+      end
+    end
+  end
+end
+
+#shoe_size is the exact same method as num_points_scored
+def shoe_size(specific_player)
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |player|
+      if player[:player_name] == specific_player
+        return player[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team)
+  # binding.pry
+  game_hash.each do |location, team_data|
+    # binding.pry
+    if team_data[:team_name] == team
+      return team_data[:colors]
+    end
+  end
+end
+
+def team_names #return array of team names [array]
+  teams_array = []  #create new array to hold data from within hash
+  game_hash.each do |location, team_data| #iterate through hash accessing data within :home and :away
+    teams_array << team_data[:team_name] #shovel team_name to teams_array
+  end
+  teams_array #return teams_array
+end
+
+def player_numbers(team_name) #method takes argument team_name
+  jersey_number = [] #creates array
+  game_hash.each do |location, team_data| #iterates to access team_data for home and away teams_array
+  if team_data[:team_name] == team_name #if team_name accessed, matches team entered in param
+    team_data[:players].each do |stats_keys| #access players statistical keys, :number, :shoe, :points. etc.
+      # binding.pry
+      jersey_number << stats_keys[:number] #shovel :number to jersey_number array
+    end
+  end
+end
+jersey_number # return new array
+end
+
+def player_stats(player)
+  new_hash = {}
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |attribute|
+      if attribute[:player_name] == player
+        new_hash[:number] = attribute[:number]
+        new_hash[:shoe] = attribute[:shoe]
+        new_hash[:points] = attribute[:points]
+        new_hash[:rebounds] = attribute[:rebounds]
+        new_hash[:assists] = attribute[:assists]
+        new_hash[:steals] = attribute[:steals]
+        new_hash[:blocks] = attribute[:blocks]
+        new_hash[:slam_dunks] = attribute[:slam_dunks]
+      end
+    end
+  end
+  new_hash
+end
+
+def big_shoe_rebounds
+  shoe_size = 0 #counter
+  rebounds = 0 #counter
+  game_hash.each do |location, team_data|
+    team_data[:players].each do |attribute| #iterating to player stats_keys
+      # binding.pry
+      if attribute[:shoe] > shoe_size #if specific_player shoe is > current shoe
+        shoe_size = attribute[:shoe] #set new value
+        rebounds = attribute[:rebounds] #set that players rebounds
+      end
+    end
+  end
+  rebounds
+end
+
+# Bonus
+
+
+# def good_practices
+#   game_hash.each do |location, team_data|
+#     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
+#     binding.pry
+#     team_data.each do |attribute, data|
+#       #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
+#       binding.pry
+#
+#       #what is 'data' at each loop throughout .each block? when will the following line of code work and when will it break?
+#       data.each do |data_item|
+#           binding.pry
+#       end
+#     end
+#   end
+# end
+# good_practices
