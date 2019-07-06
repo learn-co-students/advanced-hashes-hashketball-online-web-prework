a500fc9ad30915ea301f212cdf937b6d44fe3372
jose luis <cortezluisjose@gmail.com>
2019-07-06 03:45:29 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,9 @@
 # Write your code here!
-
+ def game_hash
+  game_hash[:home][:away]
+   :colors => [{}]
+   :players => [{:number => :shoe => :points => :rebounds => :assists => :steals => :blocks => :slam_dunks =>}]
+end
 
 
 
