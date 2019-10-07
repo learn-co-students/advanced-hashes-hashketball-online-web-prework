9f9d5e06f9adcd01e5f9b29260e23516d00964e3
Laura Hatch <laura.hatch.lh@gmail.com>
2019-10-07 00:20:02 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,118 @@
-# Write your code here!
+require 'pp'
+def game_hash # defined method
+new_hash = {# created new hash
 
+  home = { # within new hash created key called home
+      :team_name =>"Brooklyn Nets", # key within hash with team name
+      color:["Black, White"], # key with array of string
+      :player_name[ # key with array
+  :Alan_Anderson => { # key with hash
+        number:"0", # keys and value  within hash
+        shoe:"16", # keys and value  within hash
+        points:"22", # keys and value  within hash
+        rebounds:"12", # keys and value  within hash
+        assists:"12", # keys and value  within hash
+        steals:"3", # keys and value  within hash
+        blocks:"3", # keys and value  within hash
+        slam_dunks:"1", # keys and value  within hash
+      }
+  Reggie_Evans: => {
+        number:"30",
+        shoe:"14",
+        points:"12",
+        rebounds:"12",
+        assists:"12",
+        steals:"12",
+        blocks:"12",
+        slam_dunks:"7",
+      }
+:Brook_Lopez => {
+        number:"11",
+        shoe:"17",
+        points:"17",
+        rebounds:"19",
+        assists:"10",
+        steals:"3",
+        blocks:"1",
+        slam_dunks:"15",
+      }
+:Mason_Plumlee => {
+        number:"1",
+        shoe:"19",
+        points:"26",
+        rebounds:"11",
+        assists:"6",
+        steals:"3",
+        blocks:"8",
+        slam_dunks:"5",
+      }
+ :Jason_Terry => {
+        number:"31",
+        shoe:"16",
+        points:"19",
+        rebounds:"2",
+        assists:"2",
+        steals:"4",
+        blocks:"11",
+        slam_dunks:"1",
+      }
+    ]
+}
 
+  :away = {
+      :team_name => "Charlotte Hornets",
+      :color:["Turquoise, Purple"],
+  :player_name[
+  :Jeff_Adrien => {
+          number:"4",
+          shoe:"18",
+          points:"10",
+          rebounds:"1",
+          assists:"1",
+          steals:"2",
+          blocks:"7",
+          slam_dunks:"2"
+        }
 
-
-
-
-
-
-
-
+  :Bismack_Biyombo => {
+        number:"0",
+        shoe:"16",
+        points:"12",
+        rebounds:"4",
+        assists:"7",
+        steals:"22",
+        blocks:"15",
+        slam_dunks:"10",
+        }
+  :DeSagna_Diop => {
+        number:"2",
+        shoe:"14",
+        points:"24",
+        rebounds:"12",
+        assists:"12",
+        blocks:"5",
+        slam_dunks:"5",
+        }
+  :Ben_Gordon => {
+        number:"8",
+        shoe:"15",
+        points:"33",
+        rebounds:"3",
+        assists:"2",
+        steals:"1",
+        blocks:"1",
+        slam_dunks:"0",
+        }
+  :Kemba_Walker => {
+        number:"33",
+        shoe:"15",
+        points:"6",
+        rebounds:"12",
+        assists:"12",
+        steals:"7",
+        blocks:"5",
+        slam_dunks:"12",
+        }
+      ]
+    }
+}
