2fb8d998551e2d36ee4bcd84d8d0e5dc5b8f456b
Almmond Hudnall <ahudnall24@gmail.com>
2019-11-29 01:27:34 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,19 @@
+def num_points_scored
+  game_hash
+end
+
+
+player_stats("Bismak Biyombo")
+  => { :number => 0,
+        :shoe => 16,
+        :points => 12
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 22,
+        :blocks => 15,
+        :slam_dunks => 10
+      }
+
 # Write your code here!
 
 
