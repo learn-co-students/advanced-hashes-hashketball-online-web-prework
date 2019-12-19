e98d510d2d78f8ee80fd3c2f666dcc55450b92b0
Dan Foley <dan@dan-foley.com>
2019-12-19 21:08:13 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -9,67 +9,73 @@ def game_hash
   YAML.load_file('game_hash.yml')
 end
 
-def num_points_scored(player)
-  game_hash.each do |home_or_away, team_data|
-    game_hash[home_or_away].each do |team_info_keys, team_info_val|
-      game_hash[home_or_away][:players].each do |team_member|
-        if team_member[:player_name] == player
-          return team_member[:points]
+def num_points_scored(players_name)
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:points]
+          end
         end
       end
     end
   end
 end
 
-def shoe_size(player)
-  game_hash.each do |home_or_away, team_data|
-    game_hash[home_or_away].each do |team_info_keys, team_info_val|
-      game_hash[home_or_away][:players].each do |team_member|
-        if team_member[:player_name] == player
-          return team_member[:shoe]
+def shoe_size(players_name)
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player_name] == players_name
+            return player[:shoe]
+          end
         end
       end
     end
   end
 end
 
-def team_colors(team)
-  game_hash.each do |home_or_away, team_data|
-    game_hash[home_or_away].each do |team_info_k, team_info_val|
-      if game_hash[home_or_away][:team_name] == team
-        return game_hash[home_or_away][:colors]
-      end
-      # binding.pry
+def team_colors(team_name)
+  game_hash.each do |place, team|
+    if team[:team_name] == team_name
+      return team[:colors]
     end
   end
 end
 
 def team_names()
-  teams = []
-  game_hash.each do |home_or_away, team_data|
-    game_hash[home_or_away].each do |team_info_k, team_info_val|
-      if !teams.include?(game_hash[home_or_away][:team_name])
-        teams << game_hash[home_or_away][:team_name]
-      end
-      # binding.pry
-    end
+  game_hash.map do |place, team|
+    team[:team_name]
   end
-  teams
-  # binding.pry
 end
 
 def player_numbers(team_input)
-  player_numbers = []
-  game_hash.each do |home_or_away, team_data|
-    if game_hash[home_or_away][:team_name] == team_input
-      game_hash[home_or_away][:players].each do |team_member|
-        player_numbers << team_member[:number]
+  nums = []
+  game_hash.each do |place, team|
+    if team[:team_name] == team_input
+      team.each do |attribute, data|
+        if attribute == :players
+          data.each do |player|
+            nums << player[:number]
+          end
+        end
       end
     end
   end
-  player_numbers
+  nums
 end
 
 def player_stats(player_input)
-
+  new_hash = {}
+  game_hash.each do |place, team|
+    team.each do |attribute, data|
+      if attribute == :players
+        data.each do |player|
+          if player[:player]
+        end
+      end
+    end
+  end
 end
