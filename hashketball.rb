c99728a5a7c02a9db913959e20747adc7f2ac7e8
Dan Foley <dan@dan-foley.com>
2019-12-19 19:02:13 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,11 @@
-# Write your code here!
+require "yaml"
+require "pry"
 
+# lib = YAML.load_file('game_hash.yml')
 
+# pp lib
 
-
-
-
-
-
-
-
+def game_hash
+  lib = YAML.load_file('game_hash.yml')
+  binding.pry
+end
